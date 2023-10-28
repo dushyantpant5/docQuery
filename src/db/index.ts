66a5b3ac7ffc3f6515d6c997169aa22130e32181
Dushyant <dushyantpant5@gmail.com>
2023-10-28 11:47:46 +0530

    Prisma Client Fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -19,18 +19,6 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate());
-};
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 export const db = prisma;
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
